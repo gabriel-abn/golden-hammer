776d1e6a788a8e6f535d28f8df06a4fbabd95f03
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-02 14:35:11 -0300

    feat(MaintenceUseCase): :sparkles: added get all maintence service
    Operations that doesn't need a request structure are treated as services.

---
+++ src/application/services/get-all-maintences-service.ts
@@ -0,0 +1,27 @@
+import { Maintence } from "@domain/Maintence";
+import { ApplicationError } from "../common/application-error";
+import { IMaintenceRepository } from "../repositories/maintence-repository";
+
+namespace GetAllMaintences {
+  export type Response = {
+    maintences: Maintence[] | null;
+  };
+}
+
+export class GetAllMaintencesService {
+  constructor(private repository: IMaintenceRepository) {}
+
+  async execute(): Promise<GetAllMaintences.Response> {
+    try {
+      const maintences = await this.repository.getAll();
+
+      if (!maintences) {
+        throw new ApplicationError("No maintences found.", "GetAllMaintences");
+      }
+
+      return { maintences };
+    } catch (error) {
+      throw new ApplicationError("Unexpected error", "GetAllMaintences");
+    }
+  }
+}
