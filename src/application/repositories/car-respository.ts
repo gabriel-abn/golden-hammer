ce22adfaace9067111a046a98fc9190b1657ec26
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-02 16:16:34 -0300

    feat(repositories): :sparkles: introduced get by id method

--- src/application/repositories/car-respository.ts
+++ src/application/repositories/car-respository.ts
@@ -3,4 +3,5 @@ import { Car, CarProps } from "@domain/Car";
 export interface ICarRepository {
   create(car: Car): Promise<string>;
   getByPlate(plate: string): Promise<CarProps>;
+  getById(id: string): Promise<CarProps>;
 }
