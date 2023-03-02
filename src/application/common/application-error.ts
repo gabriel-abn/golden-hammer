68dd34c937b87c965b64eee2e5e65de86cb0e126
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-02 14:29:35 -0300

    feat: :art: created application error class

---
+++ src/application/common/application-error.ts
@@ -0,0 +1,8 @@
+export class ApplicationError extends Error {
+  public errorMessage: string | string[];
+  constructor(message: string | string[], className: string, name?: string) {
+    super(className);
+    this.name = name;
+    this.errorMessage = message;
+  }
+}
