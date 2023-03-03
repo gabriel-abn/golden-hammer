339eb0b2ff758cd5415a8858196c65a2dd10afbc
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-02 23:36:23 -0300

    refactor: :art: using array to show params

--- src/presentation/errors/invalid-missing-params.ts
+++ src/presentation/errors/invalid-missing-params.ts
@@ -1,7 +1,7 @@
 import { PresentationError } from "./presentation-error";
 
 export class InvalidMissingParams extends PresentationError {
-  constructor(message: string) {
+  constructor(message: string | string[]) {
     super("Invalid/Missing params: " + message, 401);
   }
 }
