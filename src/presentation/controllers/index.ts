2991d2caaf039338eaa04ddc9e55dd916cda84b1
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-06 16:48:41 -0300

    style: :art: added modular export

---
+++ src/presentation/controllers/index.ts
@@ -0,0 +1,6 @@
+export * from "./create-maintence-controller";
+export * from "./edit-maintence-controller";
+export * from "./get-all-maintences-controller";
+export * from "./get-maintence-controller";
+export * from "./register-car-controller";
+export * from "./register-client-controller";
