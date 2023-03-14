3590cc5890ee353750d14486bfcaf66e0b252359
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-13 22:43:44 -0300

    feat(repositories): :sparkles: implemented client repository methods

--- src/infra/database/repositories/client-repository.ts
+++ src/infra/database/repositories/client-repository.ts
@@ -1,11 +1,42 @@
 import { IClientRepository } from "@application/repositories";
 import { Client, ClientProps } from "@domain/Client";
+import { RelationalDatabase } from "../relational-database";
 
 export class ClientRepository implements IClientRepository {
-  create(client: Client): Promise<string> {
-    throw new Error("Method not implemented.");
+  constructor(private database: RelationalDatabase) {}
+
+  async create(client: Client): Promise<string> {
+    try {
+      const props = client.getProps();
+
+      const response = await this.database.query(`
+        INSERT INTO Client
+        (name, cpf, cnh, birthdate, email)
+        VALUES 
+        (
+          '${props.name}',
+          '${props.cpf}',
+          '${props.cnh}',
+          '${props.birthdate}',
+          '${props.email}'
+        )
+        RETURNING email
+      `);
+
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
   getByCPF(cpf: string): Promise<ClientProps> {
-    throw new Error("Method not implemented.");
+    try {
+      const response = this.database.query(`
+        SELECT * FROM Client WHERE cpf = '${cpf}';
+      `);
+
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
