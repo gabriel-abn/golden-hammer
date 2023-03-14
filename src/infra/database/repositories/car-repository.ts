68a55115772ffe7c588b7b81169b20b09db7563c
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-13 22:43:22 -0300

    feat(repositories): :sparkles: implemented car repository methods

--- src/infra/database/repositories/car-repository.ts
+++ src/infra/database/repositories/car-repository.ts
@@ -1,14 +1,53 @@
 import { ICarRepository } from "@application/repositories";
 import { Car, CarProps } from "@domain/Car";
+import { RelationalDatabase } from "@infra/database";
 
 export class CarRepository implements ICarRepository {
+  constructor(private database: RelationalDatabase) {}
+
   create(car: Car): Promise<string> {
-    throw new Error("Method not implemented.");
+    try {
+      const props = car.getProps();
+
+      const response = this.database.query(`
+        INSERT INTO Car 
+        (id, plate, color, model, brand, cpfOwner)
+        VALUES (
+          '${props.id}',
+          '${props.plate}',
+          '${props.color}',
+          '${props.model}',
+          '${props.brand}',
+          '${props.cpfOwner}'
+        )
+        RETURNING id;
+      `);
+
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
   getByPlate(plate: string): Promise<CarProps> {
-    throw new Error("Method not implemented.");
+    try {
+      const response = this.database.query(`
+        SELECT * FROM Car WHERE plate = '${plate}';
+      `);
+
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
   getById(id: string): Promise<CarProps> {
-    throw new Error("Method not implemented.");
+    try {
+      const response = this.database.query(`
+        SELECT * FROM Car WHERE id = '${id}';
+      `);
+
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
