da99c832630cda363044887beaea8585f794e3eb
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-13 22:45:21 -0300

    feat: :sparkles: created prisma orm class

--- src/infra/database/prisma/prisma-client.ts
+++ src/infra/database/prisma/prisma-client.ts
@@ -1,5 +1,18 @@
+import { RelationalDatabase } from "@infra/database";
 import { PrismaClient } from "@prisma/client";
 
-export const prismaWorker = () => {
-  const prisma = new PrismaClient();
-};
+export class PrismaDatabase implements RelationalDatabase {
+  private prisma: PrismaClient;
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+  async query(query: string): Promise<any> {
+    try {
+      const response = await this.prisma.$queryRaw`${query}`;
+
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
