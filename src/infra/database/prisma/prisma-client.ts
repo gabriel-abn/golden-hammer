86b7f72cbb5d0ebeadf5d2e4aa24d9d150a345ed
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-16 01:47:01 -0300

    feat: :sparkles: implemented execute method to prisma orm class

--- src/infra/database/prisma/prisma-client.ts
+++ src/infra/database/prisma/prisma-client.ts
@@ -8,7 +8,16 @@ export class PrismaDatabase implements RelationalDatabase {
   }
   async query(query: string): Promise<any> {
     try {
-      const response = await this.prisma.$queryRaw`${query}`;
+      const response = await this.prisma.$queryRawUnsafe(query);
+
+      return response;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+  async execute(query: string): Promise<any> {
+    try {
+      const response = await this.prisma.$executeRawUnsafe(query);
 
       return response;
     } catch (error) {
