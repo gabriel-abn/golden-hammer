208d369b2d9e7948053c8e71109ec7c422df3906
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-17 00:05:40 -0300

    feat: :art: addec delete maintence controller to modular export

--- src/presentation/controllers/index.ts
+++ src/presentation/controllers/index.ts
@@ -1,4 +1,5 @@
 export * from "./create-maintence-controller";
+export * from "./delete-maintence-controller";
 export * from "./edit-maintence-controller";
 export * from "./get-all-maintences-controller";
 export * from "./get-maintence-controller";
