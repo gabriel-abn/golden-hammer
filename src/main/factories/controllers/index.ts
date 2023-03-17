1a09c65ce3a44b4795d75713847abea94767c32c
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-17 00:13:24 -0300

    feat: :art: added controllers factory modular export

--- src/main/factories/controllers/index.ts
+++ src/main/factories/controllers/index.ts
@@ -1,3 +1,7 @@
 export * from "./create-maintence-controller-factory";
+export * from "./delete-maintence-controller-factory";
+export * from "./edit-maintence-controller-factory";
+export * from "./get-all-maintences-controller-factory";
+export * from "./get-maintence-controller-factory";
 export * from "./register-car-controller-factory";
 export * from "./register-client-controller-factory";
