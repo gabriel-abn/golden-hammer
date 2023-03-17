028fac2ad6b712510427330fdb845a7c2419a22d
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-17 00:16:15 -0300

    feat(Cars): :sparkles: added maintence routes

--- src/main/routes/maintence-routes.ts
+++ src/main/routes/maintence-routes.ts
@@ -1,7 +1,24 @@
 import { adaptRoute } from "@infra/http";
-import { makeCreateMaintenceController } from "@main/factories";
+import {
+  makeCreateMaintenceController,
+  makeDeleteMaintenceController,
+  makeEditMaintenceController,
+  makeGetAllMaintencesController,
+  makeGetMaintenceController,
+} from "@main/factories";
 import { Router } from "express";
 
 export const maintenceRoutes = Router();
 
 maintenceRoutes.post("/maintence", adaptRoute(makeCreateMaintenceController()));
+
+maintenceRoutes.get("/maintence", adaptRoute(makeGetAllMaintencesController()));
+
+maintenceRoutes.get("/maintence/:id", adaptRoute(makeGetMaintenceController()));
+
+maintenceRoutes.put("/maintence", adaptRoute(makeEditMaintenceController()));
+
+maintenceRoutes.delete(
+  "/maintence",
+  adaptRoute(makeDeleteMaintenceController())
+);
