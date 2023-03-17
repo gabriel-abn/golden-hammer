0bfe2b67f574703d2f350c468b4f59f2533dcddf
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-16 23:44:32 -0300

    feat: :art: modular exports to use case factories

--- src/main/factories/use-cases/index.ts
+++ src/main/factories/use-cases/index.ts
@@ -1,3 +1,7 @@
 export * from "./create-maintence-use-case-factory";
+export * from "./delete-maintence-use-case-factory";
+export * from "./edit-maintence-use-case-factory";
+export * from "./get-all-maintences-service-factory";
+export * from "./get-maintence-use-case-factory";
 export * from "./register-car-use-case-factory";
 export * from "./register-client-use-case-factory";
