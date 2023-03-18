0ac8d3ccf80ddfadc40233fb8141842774a598ac
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-03-18 14:47:48 -0300

    fix: :boom: changed database client

--- src/main/factories/use-cases/register-car-use-case-factory.ts
+++ src/main/factories/use-cases/register-car-use-case-factory.ts
@@ -1,12 +1,12 @@
 import { RegisterCarUseCase } from "@application/use-cases";
-import { PrismaDatabase } from "@infra/database/prisma";
+import { PostgresClient } from "@infra/database/postgres";
 import { CarRepository, ClientRepository } from "@infra/database/repositories";
 import { IDGenerator } from "@infra/validator";
 
 export const makeRegisterCarUseCase = (): RegisterCarUseCase => {
   return new RegisterCarUseCase(
-    new CarRepository(new PrismaDatabase()),
-    new ClientRepository(new PrismaDatabase()),
+    new CarRepository(new PostgresClient()),
+    new ClientRepository(new PostgresClient()),
     new IDGenerator()
   );
 };
